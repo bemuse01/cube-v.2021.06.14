efc7b8c36be80c8c04db883e1ae02870e02f567b
bemuse01 <sequeland@gmail.com>
2021-06-17 22:48:44 +0900

    Update cube.child.build.js

--- assets/js/application/cube/child/cube.child.build.js
+++ assets/js/application/cube/child/cube.child.build.js
@@ -1,5 +1,26 @@
 import * as THREE from '../../../lib/three.module.js'
 
+THREE.Object3D.prototype.updateMatrix = function () {
+
+    this.matrix.compose( this.position, this.quaternion, this.scale )
+
+    var pivot = this.pivot
+
+    if ( pivot != null ) {
+
+      var px = pivot.x, py = pivot.y,  pz = pivot.z
+      var te = this.matrix.elements
+
+      te[ 12 ] += px - te[ 0 ] * px - te[ 4 ] * py - te[ 8 ] * pz
+      te[ 13 ] += py - te[ 1 ] * px - te[ 5 ] * py - te[ 9 ] * pz
+      te[ 14 ] += pz - te[ 2 ] * px - te[ 6 ] * py - te[ 10 ] * pz
+
+    }
+
+    this.matrixWorldNeedsUpdate = true
+
+}
+
 export default class{
     constructor({group}){
         this.init()
@@ -38,7 +59,8 @@ export default class{
         for(let i = 0; i < 1; i++){
             const mesh = this.createMesh()
 
-            mesh.position.x = 200
+            mesh.position.x = 500
+            mesh.pivot = new THREE.Vector3(-500, 0, 0)
 
             this.local.add(mesh)
         }
@@ -62,6 +84,7 @@ export default class{
 
     // animate
     animate(){
-
+        const mesh = this.local.children[0]
+        mesh.rotation.y += 0.01
     }
 }
